3d17c918fa026271fa377468128e19c71114ca9b
dylan18x <gadieldylan02@gmail.com>
2024-12-02 12:09:59 -0500

    Update calculos.py

--- calculos.py
+++ calculos.py
@@ -7,3 +7,28 @@ def valorMayor(valor1: int, valor2: int, valor3: int) -> int:
     return mayorActual
 resultado = valorMayor(10, 20, 15)
 print(resultado)
+
+def determinarResultadosIMC(resultado):
+    if resultado >= 0 and resultado <16:
+        return "Delgadez severa"
+    elif resultado >= 16 and resultado <17:
+        return "Delgadez moderada"
+    elif resultado >= 17 and resultado <18.5:
+        return "Delgadez leve"
+    elif resultado >= 18.5 and resultado<25:
+        return "Peso normal"
+    elif resultado >= 25 and resultado <30:
+        return "Sobrepeso"
+    elif resultado >= 30 and resultado <35:
+        return "Obesidad grado 1"
+    elif resultado >= 35 and resultado <40:
+        return "Obesidad grado 2"
+    elif resultado >= 40:
+        return "Obesidad grado 3"
+    else:
+        return "IMC fuera de rango"
+        
+def calcularCuota(monto,interesAnual,numeroMeses):
+    interesMensual=(interesAnual/12)/100
+    coutaMensual=(monto*interesMensual)/(1-(1+interesMensual)**(-numeroMeses))
+    return coutaMensual
